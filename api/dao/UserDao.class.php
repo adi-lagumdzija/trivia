6a749e30062ae919e2836eaf49fcfbc56eb06269
adi-lagumdzija <adilag0511@gmail.com>
2021-04-05 23:56:05 +0200

    adding insert method

--- api/dao/UserDao.class.php
+++ api/dao/UserDao.class.php
@@ -11,6 +11,18 @@ public function get_user_by_id($id){
   return $this->query_unique("SELECT * FROM users WHERE id = :id", ["id" => $id]);
 }
 
+
+public function add_user($user){
+$sql = "INSERT INTO users (name, email, password, accounts_id) VALUES ( :name, :email, :password, :accounts_id)";
+$stmt= $this->connection->prepare($sql);
+$stmt->execute($user);
+$user['id'] = $this->connection->lastInsertId();
+return $user;
+}
+
+public function update_user($id, $user){
+
+}
 }
 
 
--- api/test.php
+++ api/test.php
@@ -7,7 +7,14 @@ error_reporting(E_ALL);
 require_once dirname(__FILE__)."/dao/UserDao.class.php";
 
 $user_dao = new UserDao();
-$user = $user_dao->get_user_by_id(2);
+//$user = $user_dao->get_user_by_id(2);
 //$user = $user_dao->get_user_by_email("adilag@gmail.com");
+$user1 = [
+  "name" => "Mirsad Vladic",
+  "email" => "mikuli@gmail.com",
+  "password" => "bbb",
+  "accounts_id" => 4
+];
+$user = $user_dao->add_user($user1);
 print_r($user);
 ?>
